18bf7d8fa5950fb4321558d61dd1419dfa28ca07
LeTrung <kenrock585@gmail.com>
2023-04-20 16:42:34 +0700

    Add project files.

---
+++ DemoWeb/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        public ActionResult TrangChu()
+        {
+            return View();
+        }
+
+        public ActionResult DangKy()
+
+        {
+            return View();
+        }
+        public ActionResult DesktopComputer()
+        {
+            return View();
+        }
+        public ActionResult ThongTin()
+        {
+            return View();
+        }
+    
+    }
+}
---
+++ DemoWeb/Controllers/UsersController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using DemoWeb.Models;
+
+
+namespace DemoWeb.Controllers
+{
+    public class UsersController : Controller
+    {
+
+        private DBSportStoreEntities database = new DBSportStoreEntities();
+
+        // GET: Users
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(Customer cust)
+        {
+            if (ModelState.IsValid)
+            {
+                if (string.IsNullOrEmpty(cust.NameCus))
+                    ModelState.AddModelError(string.Empty, "Tên đăng nhập không được để trống");
+                if (string.IsNullOrEmpty(cust.PassCus))
+                    ModelState.AddModelError(string.Empty, "Mật khẩu không được để trống");
+                if (string.IsNullOrEmpty(cust.EmailCus))
+                    ModelState.AddModelError(string.Empty, "Email không được để trống");
+                if (string.IsNullOrEmpty(cust.PhoneCus))
+                    ModelState.AddModelError(string.Empty, "Điện thoạikhông được để trống");
+                //Kiểm tra xem có người nào đã đăng kí với tên đăng nhập này hay chưa
+                var khachhang = database.Customers.FirstOrDefault(k => k.NameCus == cust.NameCus);
+                if (khachhang != null)
+                    ModelState.AddModelError(string.Empty, "Đã có người đăng kí tên này");
+                if (ModelState.IsValid)
+                {
+                    database.Customers.Add(cust);
+                    database.SaveChanges();
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            return RedirectToAction("Login");
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Customer cust)
+        {
+            if (ModelState.IsValid)
+            {
+                if (string.IsNullOrEmpty(cust.NameCus))
+                    ModelState.AddModelError(string.Empty, "Tên đăng nhập không được để trống");
+                if (string.IsNullOrEmpty(cust.PassCus))
+                    ModelState.AddModelError(string.Empty, "Mật khẩu không được để trống");
+                if (ModelState.IsValid)
+                {
+                    //Tìm khách hàng có tên đăng nhập và password hợp lệ trong CSDL
+                    var khachhang = database.Customers.
+                        FirstOrDefault(k => k.NameCus == cust.NameCus && k.PassCus == cust.PassCus);
+                    if (khachhang != null)
+                    {
+                        ViewBag.ThongBao = "Chúc mừng đăng nhập thành công";
+                        //Lưu vào session
+                        Session["Taikhoan"] = khachhang;
+                    }
+                    else
+                        ViewBag.ThongBao = "Tên đăng nhập hoặc mật khẩu không đúng";
+                }
+            }
+            return View();
+
+        }
+    }
+
+}
